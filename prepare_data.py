54574de376efd5bc2cde2e779af2133c36a065b8
jvmancuso <jason@manc.us>
2018-05-11 00:05:31 -0400

    Restruct (#1)
    * started restructuring
    
    * clean gitignore
    
    * remove extraneous models
    
    * rename main to train_judge, create empty mcts file
    
    * convert prepare_data
    
    * completed basic restruct

--- prepare_data.py
+++ prepare_data.py
@@ -5,84 +5,69 @@ from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 from torchvision import datasets, transforms
 
-# Could've used a TensorDataset here, but made this for demonstrative purposes
-class KaggleMNIST(Dataset):
-    """Kaggle version of MNIST dataset"""
-    def __init__(self, root, train=True, transform=None):
-        file = 'train.npy' if train else 'test.npy'
-        path = os.path.join(root, file)
-        self._train = train
-        self._data = np.load(path)
-        self._transform = transform
-    
-    def __len__(self):
-        """Determines the length of the dataset"""
-        return len(self._data)
-    
+class SparseMNIST(datasets.MNIST):
+    def __init__(self, pixels, n, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pixels = pixels
+        self._n = n
+        self._generator = torch.default_generator
+        
     def __getitem__(self, ix):
-        """Instructs how to retrieve a single instance from the dataset"""
-        if self._train:
-            feats = self._data[ix, 1:]
-            label = torch.tensor(self._data[ix, 0], dtype=torch.int64)
+        if self.train:
+            img, target = self.train_data[ix], self.train_labels[ix]
         else:
-            feats = self._data[ix, :]
-            label = -1
-        img = torch.from_numpy(feats).type(torch.float32).view(1, 28, 28)
-        if self._transform is not None:
-            img = self._transform(img)
-        return img.view(28 * 28), label
+            img, target = self.test_data[ix], self.test_labels[ix]
+            
+        img = img.unsqueeze(0).type(torch.float32) / 255
+        sparse = img.clone()
+        mask, pixels = self._get_mask(img)
+        sparse[~mask] = 0
 
+        if self.transform is not None:
+            img = self.transform(img)
+            sparse = self.transform(sparse)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return torch.cat((mask.type(torch.float32), sparse)), img, target
+
+    def __len__(self):
+        return self._n
+
+    def _get_mask(self, img):
+        mask = torch.zeros_like(img, dtype=torch.uint8)
+        width = img.size()[1]
+        pixels = self._get_pixels(img)
+        
+        for pixel in pixels:
+            pixel_x = pixel.item() % width
+            pixel_y = pixel.item() // width
+            mask[0][pixel_y][pixel_x] = 1
+        return mask, pixels
+
+    def _get_pixels(self, img):
+        nonzero = (img > 0).view(-1)
+        indices = torch.arange(len(nonzero))[nonzero].type(torch.long)
+        choices = torch.multinomial(torch.tensor([1] * indices.size(0), dtype=torch.float), self.pixels, generator=self._generator)
+        return indices.index_select(0, choices)
 
 def prepare_data(args):
-    """Prepare Kaggle version of MNIST dataset with optional validation split"""
-    process_csv(args.data_folder) # converts 
+    """Prepare SparseMNIST DataLoaders for training/test and optional validation"""
+    # Datasets
+    train_set = SparseMNIST(args.pixels, args.batches, args.data_folder, train=True)
+    if args.evaluate:
+        val_set = SparseMNIST(args.pixels, args.val_batches, args.data_folder, train=True)
+    test_set = SparseMNIST(args.pixels, args.test_batches, args.data_folder, train=False)
 
-    # create normalized dataset objects
-    mnist_transform = transforms.Normalize((0.1307,), (0.3081,))
-    train_set = KaggleMNIST(args.data_folder, train=True, transform=mnist_transform)
-    val_set = KaggleMNIST(args.data_folder, train=True, transform=mnist_transform)
-    test_set = KaggleMNIST(args.data_folder, train=True, transform=mnist_transform)
-    
-    # get "masks" in order to split the dataset into subsets
-    num_train = len(train_set)
-    indices = np.arange(num_train)
-    mask = np.random.sample(num_train) < args.test_split
-    other_ix = indices[~mask]
-    other_mask = np.random.sample(np.sum(~mask)) < args.train_split
-    train_ix = other_ix[other_mask]
-    test_ix = indices[mask]
-    if not np.all(other_mask):
-        val_ix = other_ix[~other_mask]
-    else:
-        val_ix = None
-
-    # use a sampler to specify which subsets correspond to training, validation, test
-    train_sampler = SubsetRandomSampler(train_ix)
-    val_sampler = SubsetRandomSampler(val_ix) if val_ix is not None else None
-    test_sampler = SubsetRandomSampler(test_ix)
-
-    kwargs = {'num_workers': 2} # specifies number of subprocesses to use for loading data
+    # DataLoaders
+    kwargs = {'num_workers': 1}
     if args.use_cuda:
-        kwargs['pin_memory'] = True # speeds up cpu to gpu retrieval
-
-    # need to include the samplers to make sure dataloaders pull from correct subsets
-    train_loader = DataLoader(train_set, batch_size=args.batch_size,
-        sampler=train_sampler, **kwargs)
-    val_loader = DataLoader(val_set, batch_size=args.test_batch_size,
-        sampler=val_sampler, **kwargs) if val_ix is not None else None
-    test_loader = DataLoader(train_set, batch_size=args.test_batch_size,
-        sampler=test_sampler, **kwargs)
+        kwargs['pin_memory'] = True
+    train_loader = DataLoader(train_set, batch_size=args.batch_size, **kwargs)
+    if args.evaluate:
+        val_loader = DataLoader(val_set, batch_size=args.eval_batch_size, **kwargs)
+    else:
+        val_loader = None
+    test_loader = DataLoader(test_set, batch_size=args.eval_batch_size, **kwargs)
 
     return train_loader, val_loader, test_loader
-
-def process_csv(root='./data/'):
-    """
-    Converts csv to numpy array and stores it for future runs.
-    Useful for future runs, since loading .npy is faster than loading from .csv.
-    """
-    for pickle_file in ['train.npy', 'test.npy']:
-        if os.path.isfile(root + pickle_file):
-            continue
-        else:
-            loaded = np.loadtxt(root + pickle_file[:-4] + '.csv', delimiter=',', skiprows=1)
-            np.save(root + pickle_file, loaded)
---
+++ train_judge.py
@@ -0,0 +1,175 @@
+from __future__ import print_function
+import os
+from datetime import datetime
+import argparse
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import transforms
+
+
+from prepare_data import prepare_data
+from model import Judge
+from utilities import save_checkpoint, mkdir_p
+
+
+def main(args):
+    # reproducibility
+    if args.seed is not None:
+        torch.manual_seed(args.seed) # don't think this works with SparseMNIST right now
+        np.random.seed(args.seed)
+    if not os.path.isdir(args.checkpoint):
+        mkdir_p(args.checkpoint)
+    checkpoint_file = args.checkpoint + str(datetime.now())[:-10]
+
+    # cuda
+    args.use_cuda = not args.no_cuda and torch.cuda.is_available()
+    device = torch.device("cuda" if args.use_cuda else "cpu")
+
+    # eval?
+    args.evaluate = args.val_batches > 0
+
+    # prep sparse mnist
+    if not args.evaluate:
+        train_loader, _, test_loader = prepare_data(args)
+    else:
+        train_loader, val_loader, test_loader = prepare_data(args)
+
+    # machinery
+    model = Judge().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+
+    # setup validation metrics we want to track for tracking best model over training run
+    best_val_loss = float('inf')
+    best_val_acc = 0
+
+    print('\n================== TRAINING ==================')
+    model.train() # set model to training mode
+
+    # set up training metrics we want to track
+    correct = 0
+    train_num = len(train_loader.sampler)
+
+    for ix, (sparse, img, label) in enumerate(train_loader): # iterate over training batches
+        sparse, label = sparse.to(device), label.to(device) # get data, send to gpu if needed
+        optimizer.zero_grad() # clear parameter gradients from previous training update
+        logits = model(sparse) # forward pass
+        loss = F.cross_entropy(logits, label) # calculate network loss
+        loss.backward() # backward pass
+        optimizer.step() # take an optimization step to update model's parameters
+
+        pred = logits.max(1, keepdim=True)[1] # get the index of the max logit
+        correct += pred.eq(label.view_as(pred)).sum().item() # add to running total of hits
+
+        if ix % args.log_interval == 0: # maybe log current metrics to terminal
+            print('Train: [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tAccuracy:{:.2f}%'.format(
+                (ix + 1) * len(sparse), train_num, 100. * ix / len(train_loader), loss.item(),
+                100. * correct / ((ix + 1) * len(sparse))))
+
+    print('Train Accuracy: {}/{} ({:.0f}%)\n'.format(
+        correct, train_num, 100. * correct / train_num))
+
+    if args.evaluate:
+        print('\n================== VALIDATION ==================')
+        model.eval()
+
+        # set up validation metrics we want to track
+        val_loss = 0.
+        val_correct = 0
+        val_num = len(val_loader.sampler)
+
+        # disable autograd here (replaces volatile flag from v0.3.1 and earlier)
+        with torch.no_grad():
+            for sparse, img, label in val_loader:
+                sparse, label = sparse.to(device), label.to(device)
+                logits = model(sparse)
+
+                val_loss += F.cross_entropy(logits, label, size_average=False).item()
+
+                pred = logits.max(1, keepdim=True)[1]
+                val_correct += pred.eq(label.view_as(pred)).sum().item()
+
+        # update current evaluation metrics
+        val_loss /= val_num
+        val_acc = 100. * val_correct / val_num
+        print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            val_loss, val_correct, val_num, val_acc))
+
+        is_best = val_acc > best_val_acc
+        if is_best:
+            best_val_acc = val_acc
+            best_val_loss = val_loss # note this is val_loss of best model w.r.t. accuracy,
+                                     # not the best val_loss throughout training
+
+        # create checkpoint dictionary and save it;
+        # if is_best, copy the file over to the file containing best model for this run
+        state = {
+            'state_dict': model.state_dict(),
+            'optimizer_state': optimizer.state_dict(),
+            'val_loss': val_loss,
+            'val_acc': val_acc,
+        }
+        save_checkpoint(state, is_best, checkpoint_file)
+
+    print('\n================== TESTING ==================')
+    check = torch.load(checkpoint_file + '-best.pth.tar')
+    model.load_state_dict(check['state_dict'])
+    model.eval()
+
+    test_loss = 0.
+    test_correct = 0
+    test_num = len(test_loader.sampler)
+
+    # disable autograd here (replaces volatile flag from v0.3.1 and earlier)
+    with torch.no_grad():
+        for sparse, img, label in test_loader:
+            sparse, label = sparse.to(device), label.to(device)
+            logits = model(sparse)
+            test_loss += F.cross_entropy(logits, label, size_average=False).item()
+            pred = logits.max(1, keepdim=True)[1] # get the index of the max logit
+            test_correct += pred.eq(label.view_as(pred)).sum().item()
+
+    test_loss /= test_num
+    print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, test_correct, test_num,
+        100. * test_correct / test_num))
+
+    print('Final model stored at "{}".'.format(checkpoint_file + '-best.pth.tar'))
+
+
+if __name__ == '__main__':
+    # parses arguments when running from terminal/command line
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example Training')
+    # Training settings/hyperparams
+    parser.add_argument('--pixels', type=int, required=True, metavar='C',
+                        help='number of nonzero pixels to remain in sparse image')
+    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                        help='input batch size for training (default: 128)')
+    parser.add_argument('--eval-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for evaluation (default: 1,000)')
+    parser.add_argument('--batches', type=int, default=30000, metavar='N',
+                        help='number of batches to train on (default: 30,000')
+    parser.add_argument('--val-batches', type=int, default=0, metavar='P',
+                        help='batches to use for validation (default: 0)')
+    parser.add_argument('--test-batches', type=float, default=400, metavar='P',
+                        help='percent of non-test data to use for training (default: 400)')
+    parser.add_argument('--lr', type=float, default=1e-4, metavar='LR',
+                        help='learning rate (default: 1e-4)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=None, metavar='S',
+                        help='random seed (default: None)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='log training status every N batches (default: 10)')
+    parser.add_argument('--data-folder', type=str, default='./data/', metavar='PATH',
+                        help='root path for folder containing MNIST data download \
+                        (default: ./data/)')
+    parser.add_argument('--checkpoint', type=str, default='./checkpoint/', metavar='PATH',
+                        help='root path for folder containing model checkpoints \
+                        (default: ./checkpoint/)')
+    args = parser.parse_args()
+
+    main(args)
